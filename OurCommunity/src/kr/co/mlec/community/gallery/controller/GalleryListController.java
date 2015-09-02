cc67c6d31422bfc00c495f065c28518bfa0787bc
yourjae <yourjae@0d7e11de-6d1e-0510-af9b-93416da89d02>
2015-09-02 11:45:01 +0000

    2015-09-02 08:44

---
+++ OurCommunity/src/kr/co/mlec/community/gallery/controller/GalleryListController.java
@@ -0,0 +1,44 @@
+package kr.co.mlec.community.gallery.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kr.co.mlec.community.gallery.dao.GalleryDAO;
+import kr.co.mlec.community.gallery.vo.GalleryVO;
+
+@WebServlet("/gallery/list")
+public class GalleryListController extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html; charset=UTF-8");
+	
+		GalleryDAO dao = new GalleryDAO();
+		
+		try {
+			ArrayList<GalleryVO> list =  dao.selectVO();
+			
+			req.setAttribute("list", list);
+			
+			RequestDispatcher rd = req.getRequestDispatcher("/jsp/community/gallery/list.jsp");
+			rd.forward(req, resp);
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+
+	
+	
+}
